a459bd07cddc8efa78513a825b5163484c21404c
TartPanther47 <cfmgp@hotmail.com>
2018-04-12 14:55:19 -0400

    Added resources to project • Added textures • Added "PieceMobile" class

---
+++ Stratego/ClassesMetier/PieceMobile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stratego
+{
+    public abstract class PieceMobile : Piece
+    {
+        public int Force { get; private set; }
+        public PieceMobile(Couleur couleurPiece, int forcePiece) : base(couleurPiece)
+        {
+            Force = forcePiece;
+        }
+    }
+}
