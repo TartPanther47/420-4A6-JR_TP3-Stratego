c2f2e77cd4cf9be0a0173bd8ac9ba5ed9c68df06
Clément <cfmgp@hotmail.com>
2018-04-17 08:23:30 -0400

    Added equality operator in Coordonnee.cs

--- Stratego/ClassesMetier/Coordonnee.cs
+++ Stratego/ClassesMetier/Coordonnee.cs
@@ -8,17 +8,48 @@ namespace Stratego
 {
    public class Coordonnee
    {
-      #region Attributs
-      public int X { get; set; }
-      public int Y { get; set; }
-      #endregion
+        #region Statiques
 
-      #region Contructeur
-      public Coordonnee(int x, int y)
-      {
-         X = x;
-         Y = y;
-      }
-      #endregion
-   }
+        public static bool operator ==(Coordonnee a, Coordonnee b) => a.X == b.X && a.Y == b.Y;
+        public static bool operator !=(Coordonnee a, Coordonnee b) => a.X != b.X || a.Y != b.Y;
+
+        #endregion
+
+        #region Attributs
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        #endregion
+
+        #region Contructeur
+
+        public Coordonnee(int x, int y)
+        {
+           X = x;
+           Y = y;
+        }
+
+        #endregion
+
+        #region Methodes
+
+        public override bool Equals(object obj)
+        {
+            var coordonnee = obj as Coordonnee;
+            return coordonnee != null &&
+                   X == coordonnee.X &&
+                   Y == coordonnee.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1861411795;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            return hashCode;
+        }
+
+        #endregion
+    }
 }
