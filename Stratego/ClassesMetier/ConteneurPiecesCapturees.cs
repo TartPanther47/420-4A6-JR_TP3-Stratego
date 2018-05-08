781d9ac41550c434539746b79872c84a43e2085b
TartPanther47 <cfmgp@hotmail.com>
2018-05-08 07:37:19 -0400

    1819a

---
+++ Stratego/ClassesMetier/ConteneurPiecesCapturees.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Stratego
+{
+    public class ConteneurPiecesCapturees
+    {
+        private List<PieceCapturee> PiecesCapturees { get; set; }
+        private StackPanel StpParent { get; set; }
+
+        public ConteneurPiecesCapturees(StackPanel stpParent)
+        {
+            PiecesCapturees = new List<PieceCapturee>();
+            StpParent = stpParent;
+        }
+
+        public void AjouterPiece(Piece piece)
+        {
+            bool bTrouve = false;
+            foreach (PieceCapturee p in PiecesCapturees)
+            {
+                if (p.Piece.GetType() == piece.GetType())
+                {
+                    p.Incrementer();
+                    bTrouve = true;
+                }
+            }
+            if (!bTrouve)
+                PiecesCapturees.Add(new PieceCapturee(piece, StpParent));
+        }
+    }
+}
---
+++ Stratego/ClassesMetier/PieceCapturee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Stratego
+{
+    public class PieceCapturee
+    {
+        private const int TAILLE_PIECES = 42;
+
+        private int NombrePieces { get; set; }
+        private Label LabelNbPieces { get; set; }
+        public Piece Piece { get; private set; }
+        
+        public PieceCapturee(Piece piece, StackPanel stpParent)
+        {
+            Piece = piece;
+
+            Grid grdCellule = new Grid();
+            grdCellule.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(TAILLE_PIECES) });
+            grdCellule.ColumnDefinitions.Add(new ColumnDefinition());
+            grdCellule.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(TAILLE_PIECES) });
+
+            grdCellule.Children.Add(new Rectangle
+            {
+                Fill = new ImageBrush(new BitmapImage(new Uri(
+                    "sprites/" + (piece.EstDeCouleur(Couleur.Rouge) ? "Rouge/" : "Bleu/") + piece.Nom + ".png",
+                    UriKind.Relative)))
+            });
+
+            LabelNbPieces = new Label()
+            {
+                Content = 1,
+                FontSize = 12
+            };
+            Grid.SetColumn(LabelNbPieces, 1);
+            grdCellule.Children.Add(LabelNbPieces);
+
+            stpParent.Children.Add(grdCellule);
+        }
+
+        public void Incrementer() => LabelNbPieces.Content = (int)LabelNbPieces.Content + 1;
+    }
+}
